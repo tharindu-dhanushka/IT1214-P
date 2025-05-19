1b1335dc85b64804b07a9abc81591067dc43eb1d
Tharindu Dhanushka <thdhanushka31@gmail.com>
2025-05-19 06:47:34 +0530

    Update and rename Practical_01 to S01/Practical_01.java

---
+++ S01/Practical_01.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.print("Hello, this is printed on a ");
+        System.out.println("single line");
+        System.out.println("This is printed on a separated Line");
+    }
+}
